7dc09d0f841a1c93222367dfe73c50f7d9637cd0
kugdev1011 <kug76501@gmail.com>
2025-04-05 01:17:28 +0900

    complete feedback

--- src/app/api/user.js
+++ src/app/api/user.js
@@ -11,3 +11,15 @@ export const signin = async (username, password) => {
     return error.response.data;
   }
 };
+
+export const updateUser = async (username, password) => {
+  try {
+    const response = await axiosApi.put("/payment/user", {
+      username: username,
+      password: password,
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
-import { Button, ButtonGroup, Card } from "@material-tailwind/react";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  IconButton,
+} from "@material-tailwind/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { logout, updateUser } from "../../redux/authSlice";
 import { useEffect, useState } from "react";
 import { getUser } from "@/app/api/payment";
-
+import { IoLogOutOutline } from "react-icons/io5";
+import { FaUserGear } from "react-icons/fa6";
+import UpdateUser from "./UpdateUser";
 const Header = () => {
   const [user, setUser] = useState({});
+  const [updateOpen, setUpdateOpen] = useState(false);
+  const handleUpdateOpen = () => {
+    setUpdateOpen(!updateOpen);
+  };
   const { isAuth } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const router = useRouter();
@@ -66,10 +77,29 @@ const Header = () => {
             <div className="flex flex-col gap-2 justify-center items-center">
               <p className="">余额: ${user.usdt}</p>
               <p className="">短信价格: {user.price}$/条</p>
-              <Button onClick={handleLogout} size="md" color="green">
-                登出
-              </Button>
+              <div className="flex gap-2">
+                <IconButton
+                  onClick={handleUpdateOpen}
+                  size="md"
+                  color="green"
+                  className="flex items-center justify-center"
+                >
+                  <FaUserGear size={24} />
+                </IconButton>
+                <IconButton
+                  onClick={handleLogout}
+                  size="md"
+                  color="red"
+                  className="flex items-center justify-center"
+                >
+                  <IoLogOutOutline size={24} />
+                </IconButton>
+              </div>
             </div>
+            <UpdateUser
+              updateOpen={updateOpen}
+              handleUpdateOpen={handleUpdateOpen}
+            />
           </Card>
         </div>
       ) : (
